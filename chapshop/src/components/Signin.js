9c8f658da6a3341475023a09a6eef46fb20c0473
wanyuasam <samuelwanyua@gmail.com>
2024-08-12 15:40:36 +0300

    Create sign up and sign in form

--- chapshop/src/components/Signin.js
+++ chapshop/src/components/Signin.js
@@ -22,7 +22,7 @@ function SignIn() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm sm:text-sm"
             />
           </div>
           <div>
@@ -38,7 +38,7 @@ function SignIn() {
           </div>
           <button
             type="submit"
-            className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="w-full bg-black text-white py-2 px-4 rounded-md shadow-sm"
           >
             Sign In
           </button>
