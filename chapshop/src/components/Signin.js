eeb2e3fb0d900cc00875abea90566be8594af9b9
wanyuasam <samuelwanyua@gmail.com>
2024-08-12 15:44:56 +0300

    Debug sign up and sign in form

--- chapshop/src/components/Signin.js
+++ chapshop/src/components/Signin.js
@@ -33,7 +33,7 @@ function SignIn() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm  sm:text-sm"
             />
           </div>
           <button
--- chapshop/src/components/Signup.js
+++ chapshop/src/components/Signup.js
@@ -34,7 +34,7 @@ function SignUp() {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm  sm:text-sm"
             />
           </div>
           <div>
@@ -45,12 +45,12 @@ function SignUp() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm  sm:text-sm"
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="w-full bg-black text-white py-2 px-4 rounded-md shadow-sm"
           >
             Sign Up
           </button>
