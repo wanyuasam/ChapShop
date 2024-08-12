7ea79360f899f4c77a21cd71b6de15e4165ee3eb
wanyuasam <samuelwanyua@gmail.com>
2024-08-12 15:55:15 +0300

    Restyle the home page

--- chapshop/src/components/Home.js
+++ chapshop/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Welcome to Chap Shop</h1>
+      <h1 className="text-2xl font-bold">Welcome to Chap Shop</h1>
       <h4 className="text-l font-italic">Convenience at your door!</h4>
     </div>
   );
