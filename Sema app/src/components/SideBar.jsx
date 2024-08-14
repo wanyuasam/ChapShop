5297770caac247775ef3406b5bc2e79bf77de0d2
wanyuasam <samuelwanyua@gmail.com>
2024-08-14 13:27:45 +0300

    work on on the side bar

---
+++ Sema app/src/components/SideBar.jsx
@@ -0,0 +1,49 @@
+import {AccountBox,Article,Group,Home,ModeNight,Person,Settings,Storefront} from "@mui/icons-material";
+import {Box,List,ListItem,ListItemButton,ListItemIcon,ListItemText,Switch} from "@mui/material";
+import React, { useState } from "react";
+  
+const Sidebar = () => {
+  const [mode, setMode] = useState();
+  return (
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;
