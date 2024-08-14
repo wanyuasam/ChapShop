884abf66422a747c2f3597af97c310c08c51286a
wanyuasam <samuelwanyua@gmail.com>
2024-08-14 13:15:22 +0300

    work on on the side bar

--- chapshop/src/components/RightBar.jsx
+++ chapshop/src/components/RightBar.jsx
@@ -1,75 +1,10 @@
+import { Box } from '@mui/material'
 import React from 'react'
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import SendIcon from '@mui/icons-material/Send';
-import ScheduleSendIcon from '@mui/icons-material/ScheduleSend';
-import CancelScheduleSendIcon from '@mui/icons-material/CancelScheduleSend';
+
 
 const RightBar = () => {
   return (
-    <Box bgcolor="white" flex={2} padding={2} sx={{display: {xs: "none", sm: "block"}}}>
-        <nav aria-label="main mailbox folders">
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Inbox" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <SendIcon />
-              </ListItemIcon>
-              <ListItemText primary="Sent" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <ScheduleSendIcon />
-              </ListItemIcon>
-              <ListItemText primary="Schedule sent" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <DraftsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Drafts" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <CancelScheduleSendIcon />
-              </ListItemIcon>
-              <ListItemText primary="Archived" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </nav>
-      <Divider />
-      <nav aria-label="secondary mailbox folders">
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#simple-list">
-              <ListItemText primary="Logout" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </nav>
-    </Box>
+    <Box bgcolor="grey" flex={2} padding={2} sx={{display: {xs: "none", sm: "block"}}}>RightBar</Box>
   )
 }
 
