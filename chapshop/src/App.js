d4fc7c9d97f03fbaba7c462b43462463818cdee7
wanyuasam <samuelwanyua@gmail.com>
2024-08-14 10:55:55 +0300

    Initial commit - Sema app

--- chapshop/src/App.js
+++ chapshop/src/App.js
@@ -1,24 +1,8 @@
 import React from 'react'
-import TourCard from './components/TourCard'
-import { Button, styled } from '@mui/material'
-// import {DeleteIcon} from '@mui/icons-material/Delete';
-import DeleteIcon from '@mui/icons-material/Delete';
 const App = () => {
-  const BlueButton = styled(Button)({
-    backgroundColor: "skyblue",
-    color: "#008",
-    margin: 5,
-    "&:hover": {
-      backgroundColor: "lightblue"
-    },
-  })
+  
   return (
     <div className='App'>
-      <Button variant="text" size='small'>HDKSJF</Button>
-      <Button variant="contained" startIcon={<DeleteIcon />} color='primary'>Delete</Button>
-      <Button variant="outlined"href='https://youtube.com'>Outlined</Button>
-      <Button variant='contained'  disabled>Unique Button</Button>
-      <BlueButton>THEOIJ</BlueButton>
     </div>
   )
 }
