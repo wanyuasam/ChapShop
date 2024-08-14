3e2a7563749dd71e40a9324c27c52ccb57df9b11
wanyuasam <samuelwanyua@gmail.com>
2024-08-14 10:33:05 +0300

    Creating custom styles

--- chapshop/src/App.js
+++ chapshop/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import TourCard from './components/TourCard'
-import { Container } from '@mui/material'
-import {Grid} from '@mui/material'
+import { Button, styled } from '@mui/material'
+// import {DeleteIcon} from '@mui/icons-material/Delete';
+import DeleteIcon from '@mui/icons-material/Delete';
 const App = () => {
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "#008",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue"
+    },
+  })
   return (
     <div className='App'>
-      <Container>
-        <Grid container spacing={2}>
-          <TourCard/>
-        </Grid>
-      </Container>
+      <Button variant="text" size='small'>HDKSJF</Button>
+      <Button variant="contained" startIcon={<DeleteIcon />}>Delete</Button>
+      <Button variant="outlined"href='https://youtube.com'>Outlined</Button>
+      <Button variant='contained'  disabled>Unique Button</Button>
+      <BlueButton>THEOIJ</BlueButton>
     </div>
   )
 }
