1c1e010c72e3b3ab8a94ae36f4c7fcabb7b8478a
wanyuasam <samuelwanyua@gmail.com>
2024-08-14 12:40:49 +0300

    work on on the positioned menu

--- chapshop/src/components/Navbar.jsx
+++ chapshop/src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import { AppBar, Avatar, Badge, Box, styled, Toolbar, Typography } from '@mui/ma
 import ForumIcon from '@mui/icons-material/Forum';
 import { InputBase } from '@mui/material'
 import MailIcon from '@mui/icons-material/Mail';
-import React from 'react'
+import React, { useState } from 'react'
 import { Notifications } from '@mui/icons-material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const StyledToolBar = styled(Toolbar)({
     display: "flex",
@@ -36,6 +38,7 @@ const UserBox = styled(Box)(({theme}) => ({
     }
 }))
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
 
   return (
     <AppBar position='sticky'>
@@ -50,13 +53,31 @@ const Navbar = () => {
                 <Badge badgeContent={4} color="error">
                     <Notifications />
                 </Badge>
-                <Avatar alt="Remy Sharp" sx={{width: 36, height: 36}} src="/static/images/avatar/1.jpg" />
+                <Avatar alt="Remy Sharp" sx={{width: 36, height: 36}} src="/static/images/avatar/1.jpg" onClick={e => setOpen(true)}/>
             </Icons>
-            <UserBox>
+            <UserBox onClick={(e) => setOpen(true)}>
                 <Avatar alt="Remy Sharp" sx={{width: 36, height: 36}} src="/static/images/avatar/1.jpg" />
                 <Typography variant='span'>Alex</Typography>
             </UserBox>
         </StyledToolBar>
+        <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   )
 }
