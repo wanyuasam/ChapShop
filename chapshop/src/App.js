35e5306c019d51ae62f132fcc87ec1b3e8938c17
wanyuasam <samuelwanyua@gmail.com>
2024-08-14 10:58:45 +0300

    Initial commit - Sema app

--- chapshop/src/App.js
+++ chapshop/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import SideBar from './components/SideBar'
+import  Feed from './components/Feed'
+import RightBar from './components/RightBar'
 const App = () => {
   
   return (
     <div className='App'>
+      <SideBar/>
+      <Feed />
+      <RightBar />
     </div>
   )
 }
